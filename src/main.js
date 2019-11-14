601ddb36d2984bd83289a5d5bdd37f70f2b859f1
zhanghao@lenovoclould.com <zhanghao@lenovocloud.com>
2019-11-14 17:09:54 +0800

    搭建项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {Button, Select} from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
-
+Vue.use(Button) // 引用部分组件
+Vue.use(Select)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
